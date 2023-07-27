b5e041ec5b72a6ce0eeb13ecf1f4c64a8a78d36e
ogg <baoziyoo@gmail.com>
2023-07-27 17:12:18 +0800

    1.1.2

---
+++ src/Annotation/Rule/Confirmed.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Copyright (c) 2023. ogg. Inc. All Rights Reserved.
+ * ogg sit down and start building bugs in sunny weather.
+ * Author: Ogg <baoziyoo@gmail.com>.
+ * LastChangeTime: 2023-01-16 03:58:14
+ * ChangeTime: 2023-04-26 10:21:32
+ */
+
+declare(strict_types=1);
+
+namespace Baoziyoo\Hyperf\DTO\Validation\Annotation\Rule;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class Confirmed extends BaseValidation
+{
+    protected mixed $rule = 'confirmed';
+}
---
+++ src/Annotation/Rule/Same.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Copyright (c) 2023. ogg. Inc. All Rights Reserved.
+ * ogg sit down and start building bugs in sunny weather.
+ * Author: Ogg <baoziyoo@gmail.com>.
+ * LastChangeTime: 2023-01-16 03:58:14
+ * ChangeTime: 2023-04-26 10:21:32
+ */
+
+declare(strict_types=1);
+
+namespace Baoziyoo\Hyperf\DTO\Validation\Annotation\Rule;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class Same extends BaseValidation
+{
+    protected mixed $rule = 'same';
+
+    public function __construct(private readonly string $value, public string $messages = '')
+    {
+        $this->rule = $this->rule . ':' . $this->value;
+    }
+}
